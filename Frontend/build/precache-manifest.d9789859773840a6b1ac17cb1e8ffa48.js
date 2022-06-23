f01ef12e1c5480a6aff94ad65c29c8782295a77c
SchneFre <100712805+SchneFre@users.noreply.github.com>
2022-06-23 20:47:00 +0200

    build ordner

---
+++ Frontend/build/precache-manifest.d9789859773840a6b1ac17cb1e8ffa48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ad4db0b9eb82900bcf",
+    "url": "/static/css/main.5e4e51e6.chunk.css"
+  },
+  {
+    "revision": "19ad4db0b9eb82900bcf",
+    "url": "/static/js/main.19ad4db0.chunk.js"
+  },
+  {
+    "revision": "e69b13913c091b5fa449",
+    "url": "/static/js/1.e69b1391.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d45d5af9220114704d22f1735a3da6c5",
+    "url": "/index.html"
+  }
+];
